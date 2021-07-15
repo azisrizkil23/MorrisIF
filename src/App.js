de6c46f064d76d6c5a6ea2a0741446d2bd1ec68a
azisrizkil23 <87427832+azisrizkil23@users.noreply.github.com>
2021-07-15 09:24:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
 <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Azis Rizkil <code>src/App.js</code> Kelompok Amazon.
           </p>
           <a
             className="App-link"
